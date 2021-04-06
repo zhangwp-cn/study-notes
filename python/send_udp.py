1dc85442bcc4f9fd925cf1ec205a976fcfb65b56
zhangwp <zhangwp@bw30.com>
2021-04-06 13:47:43 +0800

    通过 socket 发送UDP数据

---
+++ python/send_udp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import socket
+
+def main():
+    # 创建一个udp套接字
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # 可以使用套接字收发数据
+    # udp_socket.sendto(内容（必须是bytes类型）, 对方的ip以及port)
+    # udp_socket.sendto(b'test', ('127.0.0.1', 8000))
+    while True:
+        # 从键盘获取数据(第二种方法)
+        send_data = raw_input('请输入要发送的内容：')
+        # 如果输入的数据是exit,那么就退出程序
+        if send_data == 'exit':
+            break
+        udp_socket.sendto(send_data.encode('utf-8'), ('127.0.0.1', 8000))
+
+    print('----22------')
+    #关闭套接字
+    udp_socket.close()
+
+if __name__ == '__main__':
+    main()

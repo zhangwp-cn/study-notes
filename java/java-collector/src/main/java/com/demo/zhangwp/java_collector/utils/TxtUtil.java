1d05d0cbb046267915336ddc6e7e9f0688843ac9
zhangwp <zhangwp@bw30.com>
2021-05-10 14:16:21 +0800

    更新文本处理工具类

---
+++ java/java-collector/src/main/java/com/demo/zhangwp/java_collector/utils/TxtUtil.java
@@ -0,0 +1,38 @@
+package com.demo.zhangwp.java_collector.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TxtUtil {
+
+    public static List<String> readTxtFile(String filePath) {
+        List<String> data = new ArrayList<>();
+        try {
+            String encoding = "GBK";
+            File file = new File(filePath);
+            if (file.isFile() && file.exists()) { //判断文件是否存在
+                InputStreamReader read = new InputStreamReader(
+                        new FileInputStream(file), encoding);//考虑到编码格式
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = null;
+                while ((lineTxt = bufferedReader.readLine()) != null) {
+                    data.add(lineTxt);
+                }
+                read.close();
+            } else {
+                System.out.println("找不到指定的文件");
+            }
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+//        System.out.println(data);
+        System.out.println();
+        return data;
+    }
+
+}
